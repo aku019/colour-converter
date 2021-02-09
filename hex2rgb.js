001821b72a56ad9918f2587af61f5cb2d92fcb62
Asish kumar Upadhyay <asishupadhyay24@gmail.com>
2021-02-10 02:06:25 +0530

    first

---
+++ hex2rgb.js
@@ -0,0 +1,42 @@
+let r = document.querySelector('#inputR');
+let g = document.querySelector('#inputG');
+let b = document.querySelector('#inputB');
+const box = document.querySelector('#co');
+const btn = document.querySelector('#convertButton');
+const op = document.querySelector('#output');
+function valid(x)
+{
+    if(x.length!=7)
+    return false;
+    if(x[0]==='#')
+    {
+        for(var i=1;i<7;i++)
+        {
+            if((x[i]>='0'&&x[i]<='9')||(x[i]>='a'&&z[i]<='f')||(x[i]>='A'&&x[i]<='F'))
+            continue;
+            else
+            return false;
+        }
+        return true;
+
+    }
+    return false;
+}
+
+btn.addEventListener('click', () => {
+    
+    if(!(valid(op.value)))
+    {
+        window.alert("Please Enter a valid hex string");
+    }
+    else{
+    // eslint-disable-next-line no-undef
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(op.value);
+
+  
+   r.value = "Red Component: " + parseInt(result[1], 16);
+   g.value = "Green Component: " + parseInt(result[2], 16);
+   b.value= "Blue Component: " + parseInt(result[3], 16);
+    box.style.backgroundColor=op.value;
+   }
+});
---
+++ rgb2hex.js
@@ -0,0 +1,31 @@
+let r = document.querySelector('#inputR');
+let g = document.querySelector('#inputG');
+let b = document.querySelector('#inputB');
+const box = document.querySelector('#co');
+const btn = document.querySelector('#convertButton');
+const op = document.querySelector('#output');
+btn.addEventListener('click', () => {
+    
+    if(!(r.value>=0&&r.value<=255&&b.value>=0&&b.value<=255&&g.value>=0&&g.value<=255))
+    {
+        window.alert("Please Enter a number between 0-255.");
+    }
+    else{
+    // eslint-disable-next-line no-undef
+    r = Number(r.value).toString(16);
+    g = Number(g.value).toString(16);
+    b = Number(b.value).toString(16);
+
+  
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+     
+    
+    op.value = "#" + r + g + b;
+    box.style.backgroundColor=op.value;
+   }
+});
